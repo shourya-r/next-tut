817413033b3e483ae610682f5642ed87e6fca24a
shourya-r <rajputshourya7@gmail.com>
2025-02-07 12:58:25 +0530

    learnt about error handling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import Hello from "../components/hello";
 
 export default function Home() {
+  //throw new Error("Not implemented");
   return (
     <>
       <h1 className="text-3xl">Welcome to nextjs!</h1>
